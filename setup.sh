202eacdaf3c22ce537453a13170e8615f069c443
Jian Huang <huangjian1993@gmail.com>
2015-08-29 21:16:49 +0800

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,11 +6,13 @@ fc-cache -vf
 
 echo "done! change the font of your terminal to YaHei Consolas Hybrid and enjoy it~"
 
+echo "mv exist vim files ..."
 if [-d ~/.vim]; then
     mv ~/.vim ~/.vim.bak
 if [-f ~/.vimrc]; then
     mv ~/.vimrc ~/.vimrc.bak
 
+echo "copying qsword's vim files ...'"
 mkdir ~/.vim
 cp ./* ~/.vim/
 ln -s ~/.vim/.vimrc ~/.vimrc
