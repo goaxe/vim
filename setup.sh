954bf40518c2144350ae4339c834f4d04f33c4a4
Jian Huang <huangjian1993@gmail.com>
2015-08-29 23:30:16 +0800

    add a poetry

--- setup.sh
+++ setup.sh
@@ -8,8 +8,6 @@ cp ~/.vim/font/YaHei Consolas Hybrid.ttf /usr/share/fonts/truetype
 echo "updateing for font cache ..."
 fc-cache -vf
 
-echo "done! change the font of your terminal to YaHei Consolas Hybrid and enjoy it~"
-
 echo "mv exist vim files ..."
 if [ -d ~/.vim ]; then
     mv ~/.vim ~/.vim.bak
@@ -23,19 +21,35 @@ mkdir ~/.vim
 cp -r ./* ~/.vim/
 cp .vimrc ~/.vimrc
 
-echo "compile YouCompleteMe ..."
-yum install -y llvm
-yum install -y boost
-yum install -y cmake
+echo -e "before use YCM, please read https://github.com/Valloric/YouCompleteMe#full-installation-guide~\n"
 
-cd /etc/yum.repos.d
-wget http://people.centos.org/tru/devtools-1.1/devtools-1.1.repo 
-yum --enablerepo=testing-1.1-devtools-6 install -y devtoolset-1.1-gcc devtoolset-1.1-gcc-c++
-/opt/centos/devtoolset-1.1/root/usr/bin/
-ln -s /opt/centos/devtoolset-1.1/root/usr/bin/* /usr/local/bin/
-hash -r
+echo -e "To me, vi is Zen. 
+To use vi is to practice zen.
+Every command is a koan.
+Profound to the user,
+unintelligible to the uninitiated.
+You discover truth every time you use it.\n"
 
-cd ~/.vim/bundle/YouCompleteMe
+echo -e "vi 之大道如我心之禅，
+vi 之漫路即为禅修，
+vi 之命令禅印于心，
+未得此道者视之怪诞，
+与之为伴者洞其真谛，
+长修此道者巨变人生。\n"
+
+#echo "compile YouCompleteMe ..."
+#yum install -y llvm
+#yum install -y boost
+#yum install -y cmake
+
+#cd /etc/yum.repos.d
+#wget http://people.centos.org/tru/devtools-1.1/devtools-1.1.repo 
+#yum --enablerepo=testing-1.1-devtools-6 install -y devtoolset-1.1-gcc devtoolset-1.1-gcc-c++
+#/opt/centos/devtoolset-1.1/root/usr/bin/
+#ln -s /opt/centos/devtoolset-1.1/root/usr/bin/* /usr/local/bin/
+#hash -r
+
+#cd ~/.vim/bundle/YouCompleteMe
 #./install.sh --clang-completer --system-libclang --system-boost
 
 exit 0
